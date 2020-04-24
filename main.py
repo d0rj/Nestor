3a65ac57cf99b26350cd4aef73944b2dfc554e49
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-04-24 20:32:30 +0300

    Добавлен GUI. Простой GUI.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pyautogui as pag
 import time
 from typing import List
+from tkinter import *
+import tkinter.filedialog as fd
+
+
+FILE_NAME = 'text.txt'
+
+
+def choose_file():
+    global FILE_NAME
+    FILE_NAME = fd.askopenfilename()
+
+
+def on_choose_click(button: Button):
+    choose_file()
+    button['text'] = FILE_NAME
 
 
 def write_lines(lines: List[str], interval: float = 0):
@@ -11,6 +26,8 @@ def write_lines(lines: List[str], interval: float = 0):
 
 
 def write_from_file(file_path: str = 'text.txt', interval: float = 0):
+    time.sleep(5)
+
     text = str()
     with open(file_path) as file:
         text = file.read()
@@ -19,9 +36,41 @@ def write_from_file(file_path: str = 'text.txt', interval: float = 0):
 
 
 def main():
-    time.sleep(5)
+    global FILE_NAME
 
-    write_from_file()
+    root = Tk()
+    root.title("Тупа пажилой паук")
+    root.geometry("400x300")
+    root.attributes("-topmost", True)
+
+    interval_label = Label(root,
+                            text='Интервал печати:',
+                            font='Arial 18')
+    interval_label.pack(side=TOP)
+
+    input_interval = StringVar()
+
+    interval_entry = Entry(root,
+                            justify=RIGHT,
+                            width=100,
+                            textvariable=input_interval,
+                            )
+    interval_entry.insert(0, '0')
+    interval_entry.pack(side=TOP)
+
+    select_button = Button(root,
+                            text='Файл: ' + FILE_NAME,
+                            background='#C0C0C0',
+                            command=lambda: on_choose_click(select_button))
+    select_button.pack(side=TOP)
+
+    work_button = Button(root, 
+                        text='Начать', 
+                        background='#696969',
+                        command=lambda: write_from_file(FILE_NAME, float(input_interval.get())))
+    work_button.pack(side=BOTTOM, fill=X)
+    
+    root.mainloop()
 
 
 if __name__ == '__main__':
