137ee316e9600159eb2b8f697f3a10118b861462
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-04-24 20:00:47 +0300

    Печать вынесена в отдельные методы

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import pyautogui as pag
 import time
+from typing import List
+
+
+def write_lines(lines: List[str], interval: float = 0):
+    for line in lines:
+        pag.write(line, interval)
+        pag.write('\n')
+        pag.press('home')
+
+
+def write_from_file(file_path: str = 'text.txt', interval: float = 0):
+    text = str()
+    with open(file_path) as file:
+        text = file.read()
+
+    write_lines(text.split('\n'), interval)
 
 
 def main():
     time.sleep(5)
 
-    text = str()
-    with open('test.txt') as file:
-        text = file.read()
-
-    lines = text.split('\n')
-
-    for line in lines:
-        pag.write(line)
-        pag.write('\n')
-        pag.press('home')
+    write_from_file()
 
 
 if __name__ == '__main__':
