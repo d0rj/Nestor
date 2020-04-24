8d65c9d3e53bd3dc382ddbfd7e1d6c5819d26957
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-04-24 19:50:56 +0300

    It's work! Печатает как в файле, без лишних отступов

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import pyautogui as pag
+import time
 
 
 def main():
-    pass
+    time.sleep(5)
+
+    text = str()
+    with open('test.txt') as file:
+        text = file.read()
+
+    lines = text.split('\n')
+
+    for line in lines:
+        pag.write(line)
+        pag.write('\n')
+        pag.press('home')
 
 
 if __name__ == '__main__':
