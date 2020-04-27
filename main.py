dd937372f9573309a0807ddbd8392bd9859441b6
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-04-27 16:54:06 +0300

    Добавлена настройка задержки перед началом переписывания. Учтены некоторые исключительные ситуации. Улучшено форматирование самого кода.

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def write_lines(lines: List[str], interval: float = 0):
         pag.press('home')
 
 
-def write_from_file(file_path: str = 'text.txt', interval: float = 0):
-    time.sleep(5)
+def write_from_file(file_path: str = 'text.txt', interval: float = 0, timeout: float = 5):
+    time.sleep(timeout)
 
     text = str()
     with open(file_path) as file:
@@ -39,13 +39,14 @@ def main():
     global FILE_NAME
 
     root = Tk()
-    root.title("Тупа пажилой паук")
+    root.title("Nestor")
     root.geometry("400x300")
     root.attributes("-topmost", True)
 
     interval_label = Label(root,
                             text='Интервал печати:',
-                            font='Arial 18')
+                            font='Arial 18'
+                            )
     interval_label.pack(side=TOP)
 
     input_interval = StringVar()
@@ -58,16 +59,38 @@ def main():
     interval_entry.insert(0, '0')
     interval_entry.pack(side=TOP)
 
+    timeout_label = Label(root,
+                            text='Задержка перед печатью:',
+                            font='Arial 18'
+                            )
+    timeout_label.pack(side=TOP)
+
+    input_timeout = StringVar()
+
+    timeout_entry = Entry(root,
+                            justify=RIGHT,
+                            width=100,
+                            textvariable=input_timeout,
+                            )
+    timeout_entry.insert(0, '5')
+    timeout_entry.pack(side=TOP)
+
     select_button = Button(root,
                             text='Файл: ' + FILE_NAME,
                             background='#C0C0C0',
-                            command=lambda: on_choose_click(select_button))
+                            command=lambda: on_choose_click(select_button)
+                            )
     select_button.pack(side=TOP)
 
     work_button = Button(root, 
                         text='Начать', 
                         background='#696969',
-                        command=lambda: write_from_file(FILE_NAME, float(input_interval.get())))
+                        command=lambda: 
+                            write_from_file(FILE_NAME,
+                                            abs(float(input_interval.get())),
+                                            abs(float(input_timeout.get()))
+                                            )
+                        )
     work_button.pack(side=BOTTOM, fill=X)
     
     root.mainloop()
