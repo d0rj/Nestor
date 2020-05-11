59757fa95abe31870a8fb51774b631b971de4c92
d0rj_3.22.8 <dima.balobin_barnaul@mail.ru>
2020-05-11 14:08:12 +0300

    Добавлен консольный режим с поддержкой команд, обновлён README

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ from tkinter import *
 import tkinter.filedialog as fd
 from tkinter import messagebox
 import os
+import sys
+import argparse
+from pathlib import Path
 
 
 FILE_NAME = 'text.txt'
+DEFAULT_INTERVAL = 0
+DEFAULT_TIMEOUT = 5
+
+
+def createParser ():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', '-s', type=Path, help='Путь к файлу с текстом, из которого нужно переписать.')
+    parser.add_argument('--clipboard', '-clip', type=bool, help='Переписать ли из буффера обмена.')
+    parser.add_argument('--interval', '-int', type=float, help='Интервал печать между каждым символом.', default=DEFAULT_INTERVAL)
+    parser.add_argument('--timeout', '-t', type=float, help='Задержка перед началом печати.', default=DEFAULT_TIMEOUT)
+ 
+    return parser
 
 
 def choose_file():
@@ -20,14 +35,14 @@ def on_choose_click(button: Button):
     button['text'] = FILE_NAME
 
 
-def write_lines(lines: List[str], interval: float = 0):
+def write_lines(lines: List[str], interval: float = DEFAULT_INTERVAL):
     for line in lines:
         pag.write(line, interval)
         pag.write('\n')
         pag.press('home')
 
 
-def write_from_file(file_path: str = 'text.txt', interval: float = 0, timeout: float = 5):
+def write_from_file(file_path: str = 'text.txt', interval: float = DEFAULT_INTERVAL, timeout: float = DEFAULT_TIMEOUT):
     if not os.path.exists(file_path):
         messagebox.showerror('Ошибка', 'Файл: \"{}\" не найден'.format(file_path))
         return
@@ -41,20 +56,24 @@ def write_from_file(file_path: str = 'text.txt', interval: float = 0, timeout: f
     write_lines(text.split('\n'), interval)
 
 
-def write_from_clipboard(root: Tk, interval: float = 0, timeout: float = 5):
+def write_from_clipboard(root: Tk, interval: float = DEFAULT_INTERVAL, timeout: float = DEFAULT_TIMEOUT):
     time.sleep(timeout)
 
     write_lines(root.clipboard_get().split('\n'), interval)
 
 
-def on_startClick(file_path: str, interval: float, timeout: float, root: Tk, use_clipboard: BooleanVar = None):
-    if bool(use_clipboard.get()):
+def work(file_path: str, interval: float, timeout: float, root: Tk, use_clipboard: bool = False):
+    if bool(use_clipboard):
         write_from_clipboard(root, interval, timeout)
     else:
         write_from_file(file_path, interval, timeout)
 
 
-def main():
+def on_startClick(file_path: str, interval: float, timeout: float, root: Tk, use_clipboard: BooleanVar = None):
+    work(file_path, interval, timeout, root, bool(use_clipboard.get()))
+
+
+def window_app_loop():
     global FILE_NAME
 
     root = Tk()
@@ -75,7 +94,7 @@ def main():
                             width=100,
                             textvariable=input_interval,
                             )
-    interval_entry.insert(0, '0')
+    interval_entry.insert(0, str(DEFAULT_INTERVAL))
     interval_entry.pack(side=TOP)
 
     timeout_label = Label(root,
@@ -91,7 +110,7 @@ def main():
                             width=100,
                             textvariable=input_timeout,
                             )
-    timeout_entry.insert(0, '5')
+    timeout_entry.insert(0, str(DEFAULT_TIMEOUT))
     timeout_entry.pack(side=TOP)
 
     select_button = Button(root,
@@ -126,6 +145,16 @@ def main():
     work_button.pack(side=BOTTOM, fill=X)
     
     root.mainloop()
+
+
+def main():
+    parser = createParser()
+    namespace = parser.parse_args(sys.argv[1:])
+
+    if namespace.source != None or namespace.clipboard != None:
+        work(namespace.source, namespace.interval, namespace.timeout, Tk(), namespace.clipboard)
+    else:
+        window_app_loop()
 
 
 if __name__ == '__main__':
